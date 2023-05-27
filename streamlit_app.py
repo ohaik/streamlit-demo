bb92969c445513284d7094e502945322bec16993
ohaik <35394127+ohaik@users.noreply.github.com>
2023-05-27 15:56:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ apps = [
     {"func": home.app, "title": "Home", "icon": "house"},
     {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
     {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
-    {"func": vector.app, "title": "vector", "icon": "bounding-box"}
+    {"func": vector.app, "title": "vector", "icon": "bounding-box"},
 
 ]
 
